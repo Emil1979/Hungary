6321729a36eea6e41dfe5599c0936e695cd68c0d
Emil <emilsamo@gmail.com>
2021-04-01 17:51:11 +0200

    first commit

---
+++ public_html/js/main.js
@@ -0,0 +1,54 @@
+
+
+$(document).ready(function () {
+
+    //ANIMATION
+    function animation() {
+        let  windowHeight = $(window).height();
+        let scroll = $(window).scrollTop();
+        $('.animation').each(function () {
+            let position = $(this).offset().top;
+            let animationName = $(this).attr('data-animation');
+            let delay = $(this).attr('data-delay');
+
+            if (position < windowHeight + scroll - 100) {
+                $(this).addClass(animationName);
+                $(this).css('animation-delay', delay);
+            }
+        });
+    }
+
+
+    $(window).scroll(function () {
+        animation();
+    });
+    animation();
+
+
+    if ($('.wellness-table-slider').length > 0) {
+        $('.wellness-table-slider').owlCarousel({
+            items: 1,
+            autoplay: true,
+            loop: true
+        });
+    }
+
+
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+    }
+
+
+});
+
